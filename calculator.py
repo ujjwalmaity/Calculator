1b773c6712f7cc8d1f8eb2c2030b3dffdd29d461
ujjwal <ujjwalmaity98@gmail.com>
2020-05-24 17:54:21 +0530

    Calculate expression

--- calculator.py
+++ calculator.py
@@ -1,20 +1,38 @@
 from tkinter import *
+import parser
 
 # FUNCTION
 i = 0
 
 
 def get_variable(num):
+    if display.get() == 'Error':
+        clear_all()
     global i
     display.insert(i, num)
     i += 1
 
 
 def get_operation(operator):
+    if display.get() == 'Error':
+        clear_all()
     global i
     length = len(operator)
     display.insert(i, operator)
     i += length
+
+
+def calculate():
+    entire_string = display.get()
+    try:
+        a = parser.expr(entire_string).compile()
+        result = eval(a)
+        clear_all()
+        display.insert(0, result)
+    except Exception as e:
+        print(e)
+        clear_all()
+        display.insert(0, 'Error')
 
 
 def clear_all():
@@ -22,6 +40,8 @@ def clear_all():
 
 
 def undo():
+    if display.get() == 'Error':
+        clear_all()
     entire_string = display.get()
     if len(entire_string):
         new_string = entire_string[:-1]
@@ -60,7 +80,7 @@ Button(root, text=')', font=('Courier New', 15, 'bold'), command=lambda: get_ope
 
 Button(root, text='AC', font=('Courier New', 15, 'bold'), bg='#d4d4d2', command=clear_all).grid(row=4, column=0, sticky=NSEW)
 Button(root, text='0', font=('Courier New', 15, 'bold'), bg='#d4d4d2', command=lambda: get_variable(0)).grid(row=4, column=1, sticky=NSEW)
-Button(root, text='=', font=('Courier New', 15, 'bold'), bg='#d4d4d2').grid(row=4, column=2, sticky=NSEW)
+Button(root, text='=', font=('Courier New', 15, 'bold'), bg='#d4d4d2', command=calculate).grid(row=4, column=2, sticky=NSEW)
 Button(root, text='/', font=('Courier New', 15, 'bold'), bg='#d4d4d2', command=lambda: get_operation('/')).grid(row=4, column=3, sticky=NSEW)
 Button(root, text='exp', font=('Courier New', 15, 'bold'), bg='#d4d4d2', command=lambda: get_operation('**')).grid(row=4, column=4, sticky=NSEW)
 Button(root, text='^2', font=('Courier New', 15, 'bold'), bg='#d4d4d2', command=lambda: get_operation('**2')).grid(row=4, column=5, sticky=NSEW)
